efa150aac18e30b5184d14576cf639939d5ffa72
ellen <ellenhalvardsson@gmail.com>
2024-02-22 14:42:15 +0100

    url fix

--- ems-frontend/src/services/BudgetService.js
+++ ems-frontend/src/services/BudgetService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const REST_API_BASE_URL = "http://localhost:8080/api/budget";
+const REST_API_BASE_URL = "http://localhost:8080/api/budgets";
 
 export const listBudgets = () => axios.get(REST_API_BASE_URL);
